e3412a84005157f4cda839cf998a4b4ae070cdf5
dongba <dltkdqls7899@naver.com>
2024-07-16 12:28:26 +0900

    static inner class

--- src/main/java/InnerTest.java
+++ src/main/java/InnerTest.java
@@ -7,10 +7,12 @@ class OutClass{
         inClass = new InClass();
     }
 
+    // instance inner class
     private class InClass{
         int iNum = 100;
 
         void inTest(){
+            // 모든 변수를 다 사용 할 수 있다.
             System.out.println("OutClass num = " +num + "(외부 클래스의 인스턴스 변수)");
             System.out.println("OutClass sNum = " + sNum + "(외부 클래스의 스태틱 변수)");
             System.out.println("InClass inNum = " + iNum + "(내부 클래스의 인스턴스 변수)");
@@ -20,12 +22,42 @@ class OutClass{
     public void usingClass(){
         inClass.inTest();
     }
+
+    // static inner class
+    static class InStaticClass{
+        int iNum = 100;
+        static int sInNum = 200;
+
+
+        void inTest(){
+            // 외부 클래스의 인스턴스 변수를 사용 할 수 없다.(num를 사용 할 수 없다.)
+
+            System.out.println("OutClass num = " + iNum + "(내부 클래스의 인스턴스 변수)");
+            System.out.println("OutClass sNum = " + sNum + "(외부 클래스의 스태틱 변수)");
+            System.out.println("InClass inNum = " + sInNum + "(내부 클래스의 스태틱 변수)");
+        }
+
+        static void sTest(){
+            // 정적 클래스의 정적 메서드에서는 내부나 외부의 static 변수만 쓸 수 있다.
+
+            System.out.println("OutClass sNum = " + sNum + "(외부 클래스의 스태틱 변수)");
+            System.out.println("InClass inNum = " + sInNum + "(내부 클래스의 스태틱 변수)");
+        }
+    }
 }
 
 public class InnerTest {
     public static void main(String[] args) {
 
-        OutClass outClass = new OutClass();
+        /*OutClass outClass = new OutClass();
         outClass.usingClass();
+         */
+
+        OutClass.InStaticClass sInClass = new OutClass.InStaticClass();
+        sInClass.inTest();
+
+        System.out.println();
+
+        OutClass.InStaticClass.sTest();
     }
 }
